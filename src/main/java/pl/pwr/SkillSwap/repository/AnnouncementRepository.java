b66b9dd1401fa70d2bb75f2c7b18b13203664e85
Zbigniew Stasienko <zbyszekstasienko@gmail.com>
2025-03-22 15:36:12 +0100

    remove custom query

--- src/main/java/pl/pwr/SkillSwap/repository/AnnouncementRepository.java
+++ src/main/java/pl/pwr/SkillSwap/repository/AnnouncementRepository.java
@@ -1,22 +1,10 @@
 package pl.pwr.SkillSwap.repository;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pl.pwr.SkillSwap.model.Announcement;
 
 @Repository
 public interface AnnouncementRepository extends JpaRepository<Announcement, Long> {
 
-    @Query(
-            value = "SELECT a FROM Announcement a " +
-                    "JOIN FETCH a.user u " +
-                    "LEFT JOIN FETCH a.skill s",
-            countQuery = "SELECT count(a) FROM Announcement a"
-    )
-    Page<Announcement> findAllWithUserAndSkill(Pageable pageable);
 }
-
-
